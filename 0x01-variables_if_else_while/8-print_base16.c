2033189a1b8b649431076b80d166b3e6998f081f
Clarion Isige <clarionisige@gmail.com>
2022-07-04 01:57:46 -0700

    Print Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdion.h>
+
+int main(void)
+{
+int i, y;
+for (i=0; i < 10; i++)
+{
+putchar (i + '0');
+}
+for (y='a'; y <= 'f'; y++)
+{
+putchar (y);
+}
+
+putchar('\n');
+return (0);
+}
