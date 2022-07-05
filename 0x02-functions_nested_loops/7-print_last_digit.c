287cfeaad0b4c186a29cba60262bdf93db8ad909
Clarion Isige <clarionisige@gmail.com>
2022-07-05 06:09:57 -0700

    All now

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,14 @@
 
 int print_last_digit(int n)
 {
-	n = n % 10;
+	int last;
 
-	if (n < 0)
-		n = -n;
-	        _putchar(n + '0');
-		return (n);
+	last = n % 10;
+
+	if (last < 0) 
+	{
+		last *= -1;
+	}
+	_putchar(last + '0');
+	return (last);
 }
