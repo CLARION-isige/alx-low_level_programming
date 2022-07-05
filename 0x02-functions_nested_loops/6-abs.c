f91c57cd971725109f46c01c23029c6df6fa3017
Clarion Isige <clarionisige@gmail.com>
2022-07-05 05:48:12 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+# include "main.h"
+/**
+  * _abs- returns absolute value
+  *@n: integer to get absolute value from
+  * Return: 0
+  */
+
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+}
