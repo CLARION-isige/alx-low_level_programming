904871faf7d52880458df7522c0a0bbf2add8424
Clarion Isige <clarionisige@gmail.com>
2022-07-05 02:19:54 -0700

    1 alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+  print alphabet
+  Return 0
+  */
+
+void print_alphabet(void)
+{
+	char  c = 'a';
+	
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
+
