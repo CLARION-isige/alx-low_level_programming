6f20e5aa27ebf85a62b16d4b5a1c5a8a70027406
Clarion Isige <clarionisige@gmail.com>
2022-07-05 04:40:06 -0700

    LOwer

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+# include <main.h>
+
+/**
+  * _islower - check lower case
+  * @c: letter
+  * Return: 0
+  */
+
+int _islower(int c)
+{
+	if (c >= 97 && c >= 122)
+		return (1);
+	else
+		return (0);
+}
