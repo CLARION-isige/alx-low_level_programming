5b588e27205f3fd7e02523984292a9f8d8b97254
Clarion Isige <clarionisige@gmail.com>
2022-07-05 03:58:58 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <ctype.c>
 
+/**
+  * main - prints putchar
+  * Return:void
+  */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('r');
+	putchar('\n');
+}
